3cf37ee75569f036d1cef667d1052a4eda374661
aluno <aluno@LB23-M06.alunos.uniceplac.edu>
2024-05-27 21:27:33 -0300

    QuickSort

---
+++ Aula0012/src/Pacote1/Modelo.java
@@ -0,0 +1,23 @@
+package Pacote1;
+
+public class Modelo {
+
+
+    public int[] numeros;
+
+    public Modelo(int tamanho) {
+        numeros = new int[tamanho];
+    }
+
+    public void adicionarNumero(int indice, int numero) {
+        numeros[indice] = numero;
+    }
+
+    public int getNumero(int indice) {
+        return numeros[indice];
+    }
+
+    public int getNumeroElementos() {
+        return numeros.length;
+    }
+}
---
+++ Aula0012/src/Pacote3/Visao.java
@@ -0,0 +1,35 @@
+package Pacote3;
+
+import java.util.Scanner;
+
+import Pacote1.Modelo;
+import Pacote2.Negocio;
+
+public class Visao {
+    public static void main(String[] args) {
+        int tamanho = 4;
+        Modelo vetor = new Modelo(tamanho);
+        Visao visualizacao = new Visao();
+        Negocio controlador = new Negocio();
+
+        visualizacao.EntrarNumero(vetor, tamanho);
+
+        long tempoInicialInsertionSort = System.currentTimeMillis();
+        controlador.insertionSort(vetor);
+        long tempoFinalInsertionSort = System.currentTimeMillis();
+        System.out.println("Insertion Sort executado em = " + (tempoFinalInsertionSort - tempoInicialInsertionSort) + " ms");
+
+        long tempoInicialQuickSort = System.currentTimeMillis();
+        controlador.quickSort(vetor,0,vetor.getNumeroElementos()-1);
+        long tempoFinalQuickSor = System.currentTimeMillis();
+        System.out.println("QuickSort executado em = " + (tempoFinalQuickSor - tempoInicialQuickSort) + " ms");
+    }
+
+    public void EntrarNumero(Modelo vetor, int tamanho) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite " + tamanho + " números:");
+        for (int i = 0; i < tamanho; i++) {
+            vetor.adicionarNumero(i, scanner.nextInt());
+        }
+    }
+}
